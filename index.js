3b0b685a1a21154e2e8e1a94c47bc74fdb413daf
your_username <bhussain.bese17seecs@seecs.edu.pk>
2021-01-24 20:13:35 +0500

    pull request

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const config = require("config");
 const app = express();
 
-
+console.log("hello")
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,7 @@ const config = require('config');
 
 module.exports = function() {
   const db = config.get('db');
+  console.log(db)
   mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => winston.info(`Connected to ${db}...`));
 }
